0272b3bcbb6ee07904c99f215f44ab185b4d18ae
Nate Stutte <ngs33@drexel.edu>
2022-05-19 16:16:22 -0400

    Moved viewpoint class to separate file

--- src/app.js
+++ src/app.js
@@ -34,7 +34,6 @@ function draw() {
     vp.moveViewpoint();
 
     ellipse(testObjX, testObjY, 40, 40);
-    ellipse(testObjX, testObjY+100, 40, 60);
     pop();
 
     // Draw crosshair in middle of screen
@@ -46,52 +45,4 @@ function draw() {
 function clicked(xpos, ypos) {
     console.log(xpos + ", " + ypos);
 }
-class viewpoint {
 
-    constructor(xloc=0, yloc=0, zoom=1) {
-        this.xloc = xloc;
-        this.yloc = yloc;
-        this.offsetX = 0;
-        this.offsetY = 0;
-        this.zoom = zoom;
-        this.dragActive = false;
-        this.clickedPossibility = false;
-    }
-
-    pressed() {
-        if (mouseX - this.offsetX != 0 || mouseY - this.offsetY != 0)
-            this.clickedPossibility = false;
-        this.xloc += mouseX - this.offsetX;
-        this.yloc += mouseY - this.offsetY;
-        this.offsetX = mouseX;
-        this.offsetY = mouseY;
-    }
-
-    released() {
-        this.dragActive = false;
-        this.offsetX = 0;
-        this.offsetY = 0;
-        if (this.clickedPossibility)
-            clicked(mouseX, mouseY);
-        this.clickedPossibility = false
-    }
-
-    moveViewpoint() {
-        if (mouseIsPressed) {
-            if (!this.dragActive) {
-                this.offsetX = mouseX;
-                this.offsetY = mouseY;
-                this.clickedPossibility = true
-                this.dragActive = true;
-            }
-            this.pressed();
-        } else {
-            this.released();
-        }
-        translate(this.xloc, this.yloc);
-    }
-
-    getViewpointLoc() {
-        return createVector(this.xloc, this.yloc);
-    }
-}
